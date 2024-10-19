f0ef6df9d62c1432735828783a158c6eb231b47e
Prince1337 <prince.pieritz@gmail.com>
2024-10-19 23:50:25 +0200

    fix: CorsFilter

--- src/main/java/evasio/config/MyCORSFilter.java
+++ src/main/java/evasio/config/MyCORSFilter.java
@@ -10,16 +10,22 @@ import java.io.IOException;
 public class MyCORSFilter extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response, jakarta.servlet.FilterChain filterChain) throws jakarta.servlet.ServletException, IOException, IOException {
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+    protected void doFilterInternal(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response, jakarta.servlet.FilterChain filterChain) throws jakarta.servlet.ServletException, IOException {
+        String origin = request.getHeader("Origin");
+        if ("http://localhost:4200".equals(origin) || "https://evasio-landing.web.app".equals(origin)) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
         response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
+
         if ("OPTIONS".equals(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
             filterChain.doFilter(request, response);
         }
     }
+
 }
